8b5382fc96c78e8bd239a71d57238722da15c231
Carter David Henning <carterdavidhenning@gmail.com>
2021-12-01 22:39:30 -0500

    Add file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::io::*;
+
 fn main() {
     println!("Hello, world!");
+
+    // Read in the file as array of ints
+    let filepath = "data/day_1_input.txt";
+    let file = File::open(filepath).unwrap();
+    let reader = BufReader::new(file);
+
+    // Iterate over each line
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let num = line.parse::<i32>().unwrap();
+        println!("{}", num);
+    }
+
+    // Track the previous line's value
+    // Add to increase or decrease tally
 }
