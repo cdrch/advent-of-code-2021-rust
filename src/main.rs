fdbd6056ce23b64e9b01079db7e5d541e40c86eb
Carter David Henning <carterdavidhenning@gmail.com>
2021-12-01 23:20:27 -0500

    Add first attempt at day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
         numbers.push(num);
     }
 
-    let depth_increases = count_depth_increases(numbers);
+    let depth_increases = count_depth_increases_with_sliding_window(numbers, 3);
     println!("{}", depth_increases);
 }
 
@@ -31,5 +31,33 @@ fn count_depth_increases(input: Vec<i32>) -> i32 {
         }
         last_depth = depth;
     }
+    increases
+}
+
+fn count_depth_increases_with_sliding_window(input: Vec<i32>, window_size: usize) -> i32 {
+    let mut increases = -1; // Start at -1 to account for the first value increasing from 0
+    let mut last_depth = 0;
+
+    for (depth, i) in input.iter().enumerate() {
+        // Check if a new window is possible
+        if i + 2 >= input.len().try_into().unwrap() {
+            break;
+        }
+        // Create sliding window
+        let mut window = Vec::new();
+        for n in 0..3 {
+            let item: usize = (i + n).try_into().unwrap();
+            window.push(&input[item]);
+        }
+
+        // Check if the window is increased from previous
+        if window[0] + window[1] + window[2] > last_depth {
+            increases += 1;
+        }
+
+        // Always save last depth
+        last_depth = window[0] + window[1] + window[2];
+    }
+
     increases
 }
